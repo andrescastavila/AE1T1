f8f1bb62a5fea6dcdba361599887b275f87eddd6
LAPTOP-FVG63UF4\andre <andrescastano93@gmail.com>
2021-09-22 19:10:04 +0200

    commit actividad 1

--- src/es/florida/AE1T1/App.java
+++ src/es/florida/AE1T1/App.java
@@ -5,13 +5,10 @@ import java.util.Scanner;
 public class App {
 
 	public static void main(String[] args) {
-		String nom = args[0];
+		String nom = "Mundo";
 		System.out.println("Hola " + nom);
 		
-		Scanner teclado = new Scanner(System.in);
-		String entradaTeclado = teclado.nextLine();
-		System.out.println("Hola " + entradaTeclado);
-		teclado.close();
+	
 
 	}
 
