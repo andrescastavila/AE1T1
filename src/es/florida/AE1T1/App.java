342b22abccf40e72ded755cf85ac918860a2901e
LAPTOP-FVG63UF4\andre <andrescastano93@gmail.com>
2021-09-24 12:32:15 +0200

    Mitad del ejercicio2

--- src/es/florida/AE1T1/App.java
+++ src/es/florida/AE1T1/App.java
@@ -1,8 +1,6 @@
 package es.florida.AE1T1;
 
-
-
-
+import java.util.Scanner;
 
 class Mensaje{
 	String nom;
@@ -19,11 +17,35 @@ public class App {
 			
 		System.out.print(msg.nom);	
 	
+		Scanner teclado = new Scanner(System.in);
+		
+		String [] nombres=new String[6];
 		
 		
-	
 		
-	
+		System.out.print(" Ingrese un nombre:");
+		nombres[0]=teclado.next();
+
+		System.out.print(" Ingrese un nombre:");
+		nombres[1]=teclado.next();
+
+		System.out.print(" Ingrese un nombre:");
+		nombres[2]=teclado.next();
+
+		System.out.print(" Ingrese un nombre:");
+		nombres[3]=teclado.next();
+
+		System.out.print(" Ingrese un nombre:");
+		nombres[4]=teclado.next();
+
+		System.out.print(" Ingrese un nombre:");
+		nombres[5]=teclado.next();
+		
+		for(int i=0;i<6;i++) {
+			System.out.print(" "+(i+1)+" Nombre:"+nombres[i]+".");
+		}
+
+		
 
 	}
 
